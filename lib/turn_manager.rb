898bd135e715d18b4fccd6d2a6bfe021f9f5d13d
Adam Romano <manoadamro.imp@gmail.com>
2017-10-03 19:11:05 +0100

    added turn_manager

---
+++ lib/turn_manager.rb
@@ -0,0 +1,21 @@
+#
+class TurnManager
+  attr_reader :current_turn, :board, :players
+
+  def initialize(players, board)
+    @players = players
+    @board = board
+    @current_turn = 0
+  end
+
+  def turn(x, y)
+    next_turn if @board.place(x, y)
+  end
+
+  private
+
+  def next_turn
+    @players[@current_turn].plays += 1
+    @current_turn = (@current_turn + 1) % 2
+  end
+end
